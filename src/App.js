58fd9685dff762dbb60a03bc365768c9267fbff0
Kshitiz-2002 <kshitizpandey2002@gmail.com>
2024-04-23 20:13:25 +0530

    feat: init pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
-function App() {
-  return (
+const App = () => {
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<LoginPage/>}/>
+        <Route path='/signup' element={<SignupPage/>}/>
+        <Route path='/home' element={<HomePage/>}/>
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/SignupPage.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return ( 
+        <div>
+            Signup Page
+        </div>
+     );
+}
+ 
+export default HomePage;
